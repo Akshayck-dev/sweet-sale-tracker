0973fa5b23a2b177614a67e035d21a89dbe0e0d4
Akshay C k <akshack008@gmail.com>
2025-11-17 15:18:21 +0530

    Update sw-register.js

--- public/sw-register.js
+++ public/sw-register.js
@@ -1,8 +1,5 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(() => console.log('SW registered'))
-      .catch(e => console.warn('SW failed', e));
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
   });
 }
-
